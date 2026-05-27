e2d2acff5b61cdae71486f55004f2e810e1608c2
adrien-duchossoy <adrien.duchossoy@icloud.com>
2026-05-27 12:47:42 +0200

    feat: delete movie from list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,18 @@
 import movies from "../data/movies.json"
+import { useState } from 'react'
 
 
 function MovieList() {
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
+    const deleteMovie = (movieId) => {
+        const newList = moviesToDisplay.filter((movie) => movie.id !== movieId)
+        setMoviesToDisplay(newList)
+    }
+
     return (
         <>
-            {movies.map((movieObj, i, arr) => {
+            <h2>{moviesToDisplay.length} movies to display</h2>
+            {moviesToDisplay.map((movieObj, i, arr) => {
                 return (
                     <div key={movieObj.id} className="card">
                         <h3>{movieObj.title}</h3>
@@ -14,6 +22,8 @@ function MovieList() {
 
                         <p>Year: {movieObj.year}</p>
                         <p>Rating: {movieObj.rating}</p>
+
+                        <button onClick={() => deleteMovie(movieObj.id)}>Delete</button>
                     </div>
                 )
             })}
